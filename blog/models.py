e336886f8ad27798724312b8142b1aa34fbe3bf7
Sourav <sourav@xyz.com>
2021-05-08 17:08:21 +0530

    updated

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=25000)
     date = models.DateField()
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
-from blog import views
 from django.urls import path
 from . import views
 
 app_name = 'blog'
+
 urlpatterns = [
-    path('',views.all_blogs,name='all_blogs'),
-    path('<int:blog_id>/',views.detail,name='detail')
+    path('', views.all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
